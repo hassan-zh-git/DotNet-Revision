23828c49fcced9e8f051446688b00c6a21d97b84
Hassan <hassanzaheer200531@gmail.com>
2026-05-02 12:15:13 +0500

    Controller Basics

--- StudentManagementAPI/Controllers/StudentManagementAPI.cs
+++ StudentManagementAPI/Controllers/StudentManagementAPI.cs
@@ -1,5 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-namespace StudentManagementAPI.Controllers;
-[ApiController]
-[Route("api/[controller]")]
+namespace StudentManagementAPI.Controllers {
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetallStudents()
+        {
+            return Ok("All Students data>>>");
+        }
 
+        [HttpGet("{id}")]
+        public IActionResult GetStudentbyId(int id)
+        {
+            return Ok($"Student data for ID: {id}>>>");
+        }
+    }
+}
