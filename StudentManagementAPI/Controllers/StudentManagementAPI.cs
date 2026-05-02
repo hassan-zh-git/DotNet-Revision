86fb789e22f5c0cb57767baa682fa368af6bda4b
Hassan <hassanzaheer200531@gmail.com>
2026-05-02 13:48:29 +0500

    Action Method

--- StudentManagementAPI/Controllers/StudentManagementAPI.cs
+++ StudentManagementAPI/Controllers/StudentManagementAPI.cs
@@ -10,10 +10,25 @@ namespace StudentManagementAPI.Controllers {
             return Ok("All Students data>>>");
         }
 
+
         [HttpGet("{id}")]
         public IActionResult GetStudentbyId(int id)
         {
             return Ok($"Student data for ID: {id}>>>");
         }
+
+       private static  List<string> studetns  =new List<string>(){"Student1","Student2","Student3"};
+
+        [HttpPost]
+        public IActionResult AddStudent([FromBody] string name)
+        {
+            if(studetns.Count >= 5)
+            {
+                return BadRequest("Cannot add more than 5 students.");
+            }
+            studetns.Add(name);
+            return Ok("studetn added sucessfully>>>");
+        }
+
     }
 }
