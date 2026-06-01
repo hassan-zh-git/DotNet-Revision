2c93b45d9ce4d324ac215f6dde0ac96ce07ad223
Hassan <hassanzaheer200531@gmail.com>
2026-06-01 14:34:08 +0500

    LAYERED ARCHITECTURE INTRODUCTION

---
+++ StudentManagementAPI/Services/StudentService.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentManagementAPI.Services
+{
+    public class StudentService
+    {
+        public string GetMessage()
+        {
+            return "Student Service Working";
+        }
+    }
+}
