50b98026578dd0385af078c5d78b127ac498adff
Hassan <hassanzaheer200531@gmail.com>
2026-06-03 17:43:21 +0500

    DbContext + DbSet (THE HEART OF EF CORE)

---
+++ StudentManagementAPI/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentManagementAPI.Data;
+using StudentManagementAPI.Models;
+
+namespace StudentManagementAPI.Data
+{
+    public class AppDbContext : DbContext                   //DbContext describe  Database Connection Bridge
+    {
+        public AppDbContext(                             //For now think: Configuration information comes here.We'll understand DI later.
+            DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }             //db set describe tabe exist the model define the table 
+    }
+}
+
+//Inheritance
+//: DbContext
+//Means:
+//AppDbContext IS A DbContext                 ,Gets all EF Core functionality
